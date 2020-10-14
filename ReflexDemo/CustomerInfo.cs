d62498af49a9ab725330a319678ec18ce0273ae7
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-14 08:37:02 +0530

    [Tirupathi] Add. Demo code of Reflection

---
+++ ReflexDemo/CustomerInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReflectionDemo
+{
+    public class CustomerInfo
+    {
+        public int ID;
+        public string ClassName;
+        public int Id
+        {
+            get;
+            set;
+        }
+        public String Name
+        {
+            get;
+            set;
+        }
+
+        public CustomerInfo()
+        {
+            this.ID = -1;
+            this.ClassName = "";
+        }
+
+        public CustomerInfo(int ID,string ClassName)
+        {
+            this.ID = ID;
+            this.ClassName = ClassName;
+        }
+
+        public void PrintId()
+        {
+            Console.WriteLine("Id is: {0}", this.ID);
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine("Name is: {0}", this.ClassName);
+        }
+
+    }
+}
---
+++ ReflexDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReflectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomerInfo customerInfo = new CustomerInfo();
+            //customerInfo.set_Name("Hell");
+
+            ReflectionOnCustomer reflectionOnCustomer = new ReflectionOnCustomer();
+            reflectionOnCustomer.ReflectionTest();
+        }
+    }
+}
